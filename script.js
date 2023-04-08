747656dfcfe6e4c097d0b6f4019fe23026d0f9a5
IliaKhalighi <ysk.iliakhalighi@gmail.com>
2023-04-07 22:27:59 -0400

    The 'again' button works now + some bug fixes

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
 'use strict';
 
-const randomNum = Math.floor(Math.random() * 20) + 1;
+let randomNum = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+let won = false;
 
 // If 'check' clicked
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  if (!guess) {
-    setMessage('⛔ No input...');
-  } else if (guess === randomNum) {
-    setMessage('✅ Correct!!');
-    setCSS('body', 'backgroundColor', '#60b347');
-    setCSS('.number', 'width', '25rem');
-    setBoxMessage(guess);
-    if (score > highScore) {
-      highScore = score;
-      setHighScore(highScore);
-    }
-  } else if (guess !== randomNum) {
-    if (score > 1) {
-      if (guess > randomNum) {
-        setMessage('📈 Too high...');
-        setScore(--score);
-      } else if (guess < randomNum) {
-        setMessage('📉 Too low...');
-        setScore(--score);
+  if (!won) {
+    if (!guess) {
+      setMessage('⛔ No input...');
+    } else if (guess === randomNum) {
+      setMessage(`✅ Correct!!
+      Click "Again!" to play a new round`);
+      setCSS('.again', 'backgroundColor', '#e3a84f');
+      setCSS('body', 'backgroundColor', '#60b347');
+      setCSS('.number', 'width', '25rem');
+      setBoxMessage(guess);
+      won = true;
+      if (score > highScore) {
+        highScore = score;
+        setHighScore(highScore);
       }
-    } else {
-      setMessage('😢 You lost :(');
-      setCSS('body', 'backgroundColor', '#b34747');
-      setScore(0);
+    } else if (guess !== randomNum) {
+      if (score > 1) {
+        if (guess > randomNum) {
+          setMessage('📈 Too high...');
+          setScore(--score);
+        } else if (guess < randomNum) {
+          setMessage('📉 Too low...');
+          setScore(--score);
+        }
+      } else {
+        setMessage(`😢 You lost :( 
+          Click "Again!" to play a new round`);
+        setCSS('.again', 'backgroundColor', '#e3a84f');
+        setCSS('body', 'backgroundColor', '#b34747');
+        setScore(0);
+      }
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  won = false;
+  score = 20;
+  randomNum = Math.floor(Math.random() * 20) + 1;
+  setScore(score);
+  setCSS('.again', 'backgroundColor', '#eee');
+  setCSS('body', 'backgroundColor', '#222');
+  setCSS('.number', 'width', '15rem');
+  setMessage('Start guessing...');
+  setBoxMessage('?');
+  setGuessBox('');
 });
 
 const setBoxMessage = function (message) {
@@ -49,6 +70,10 @@ const setScore = function (score) {
 
 const setHighScore = function (score) {
   document.querySelector('.highscore').textContent = score;
+};
+
+const setGuessBox = function (score) {
+  document.querySelector('.guess').value = score;
 };
 
 const checkIfLost = function (score) {
